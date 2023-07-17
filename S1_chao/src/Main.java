c5cd3a0082b662fb3908a5d1288a4551e13dad81
TIENSIHUNG <binlung44qn@gmail.com>
2023-07-17 19:02:35 +0700

    up

---
+++ S1_chao/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("What your name ?");
+        String name = sc.nextLine();
+        System.out.println("Hi " + name);
+
+    }
+}
