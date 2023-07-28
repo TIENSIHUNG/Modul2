275bf807f57c9ec8fb026b8af0454259fada9301
TIENSIHUNG <binlung44qn@gmail.com>
2023-07-28 20:21:40 +0700

    bump

--- S6_ke_thua/triangle/src/Test.java
+++ S6_ke_thua/triangle/src/Test.java
@@ -6,7 +6,7 @@ public class Test {
         Triangle t1 = new Triangle(2,2,3);
         System.out.println(t1.isTriangle());
         System.out.println(t1.getPerimeter());
-        System.out.println(t1.getPerimeter());
+        System.out.println(t1.getArea());
         System.out.println(t1.toString());
     }
 }
